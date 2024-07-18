61e712d943da53ec1677cab9a2753229e531d69a
Bharathi414 <bharathieie02@gmail.com>
2024-07-18 10:28:51 +0530

    modified some changes

--- backend/controllers/taskcontroller.js
+++ backend/controllers/taskcontroller.js
@@ -78,10 +78,9 @@ const deletetask = async (req,res) => {
         const task = await taskmodel.findByIdAndDelete(id);
         res.status(200).json(task)
     }
-
     catch (e){
         res.status(400).json({error : e.message})
     }
-}
+};
 
 module.exports = {createtask, gettasks, getsingletask, updatetask, deletetask}; 
